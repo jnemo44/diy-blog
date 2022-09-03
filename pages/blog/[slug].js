a915a58d8fbdfeb968bc08d86c0ed6b32b8df966
Joe Niemiec <joe.niemiec@gmail.com>
2022-09-03 14:28:58 -0500

    Basic functionality established

---
+++ pages/blog/[slug].js
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import { blogPosts } from '../../lib/data';
+
+export default function BlogPage({ title, date, content }) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>{title}</h1>
+                <div>{content}</div>
+            </main>
+
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    console.log('hi', context)
+    const { params } = context;
+    return {
+        props: blogPosts.find(post => post.slug === params.slug), // will be passed to the page component as props
+    }
+}
+
+export async function getStaticPaths() {
+
+    return {
+        paths: blogPosts.map((post) => ({
+            params: {
+                slug: post.slug,
+            },
+        })),
+        fallback: false, // can also be true or 'blocking'
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { blogPosts } from '../lib/data'
 
 export default function Home() {
   return (
@@ -12,6 +14,19 @@ export default function Home() {
       <main>
         <h1>My Blog</h1>
       </main>
+
+      <div>
+        {blogPosts.map((post) =>
+          <div key={post.slug}>
+            <div><Link href={`/blog/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+            </div>
+            <div>{post.date}</div>
+            <div>{post.content}</div>
+          </div>
+        )}
+      </div>
 
     </div>
   )
