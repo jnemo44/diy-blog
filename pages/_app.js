6a21529edc70e468c25a61ec3062b8c7d60d40c4
Joe Niemiec <joe.niemiec@gmail.com>
2022-09-28 20:12:03 -0500

    minor styling updates

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,12 @@ function MyApp({ Component, pageProps, test }) {
   let currentDate = Date.now()
   let raceDate = new Date("12/4/2022")
   return (
-    <div className="mx-auto w-8/12 font-Poppins">
+    <div className="mx-auto w-10/12 sm:w-8/12  font-Poppins">
       <header className='flex w-full justify-between space-x-4'>
         <Link href="/"><a className="text-2xl font-bold my-4 whitespace-nowrap">Joe Niemiec</a></Link>
         <div className="hidden sm:flex place-self-center whitespace-nowrap">Next race: {Math.round((raceDate - currentDate) / (1000 * 3600 * 24))} days </div>
         <nav className="place-self-center pr-2">
-          <div className="flex space-x-2 sm:space-x-4 font-bold">
+          <div className="flex space-x-4 font-bold">
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
 
