08fc039713655bcfd9a53f511c0a12894e2ab5d4
Joe Niemiec <joe.niemiec@gmail.com>
2022-09-30 20:47:23 -0500

    Added twitter card info

--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,36 @@
 import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-  } from 'next/document'
-  
-  class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-      const initialProps = await Document.getInitialProps(ctx)
-  
-      return initialProps
-    }
-  
-    render() {
-      return (
-        <Html>
-          <Head>
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
           <link rel="shortcut icon" href="../favicon.ico"></link>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-          </Head>
-          <body>
-            <Main/>
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@joe_niemiec" />
+          <meta name="twitter:title" content="Joe Niemiec" />
+          <meta name="twitter:description" content="Exploring the web while running for days" />
+          <meta name="twitter:image" content="/joeRunning.JPG" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
-  
-  export default MyDocument
+}
+
+export default MyDocument
