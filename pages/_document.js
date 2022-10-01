b30e7210f5218073e149957f0ccc3061f67cc3f7
Joe Niemiec <joe.niemiec@gmail.com>
2022-09-30 20:57:25 -0500

    Edit meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="../favicon.ico"></link>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@joe_niemiec" />
           <meta name="twitter:title" content="Joe Niemiec" />
           <meta name="twitter:description" content="Exploring the web while running for days" />
