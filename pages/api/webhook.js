26489c99990291c0fa065ef8592e89ef1e70332f
Joe Niemiec <joe.niemiec@gmail.com>
2022-10-01 21:13:29 -0500

    Adding webhook subscription to api for strava webhooks

---
+++ pages/api/webhook.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Process a GET request
+    // Parses the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Verifies that the mode and token sent are valid
+      if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.json({ "hub.challenge": challenge });
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+
+  } else {
+    // Handle any other HTTP method
+  }
+}
+
+
