9ffa06678618c10fc469b242a2cc41e92a45147c
Joe Niemiec <joe.niemiec@gmail.com>
2022-10-17 22:18:15 -0500

    Major update to webhook that pushes weather to new strava activities

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,7 +2,7 @@
 import db from "../../lib/db";
 import { getWeather } from "../../lib/util";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   var currentDate = new Date().toString()
   console.log("WEBHOOK FIRED")  
   if (req.method === 'GET') {
@@ -26,22 +26,57 @@ export default function handler(req, res) {
         }
       }
     } else if (req.method === 'POST') {
-      //3a36c3484bf9289bb1918fbb98150a2f098f2aca
       // Handle webhook PUSH
-      // db.collection('strava_data')
-      // .doc('hP8d1Y61Id6uQ5B7DgEW')
-      // .update({
-      //   object_id: req.body.object_id,
-      //   object_type: req.body.object_type,
-      //   aspect_type: req.body.aspect_type,
-      //   event_time: req.body.event_time,
-      //   owner_id: req.body.owner_id,
-      //   subscription_id: req.body.subscription_id,
-      //   update_date: currentDate,
-      //   updates: req.body?.updates,
-      // })
-      console.log(req.body)
-      //getWeather()
+      db.collection('strava_data')
+      .doc('hP8d1Y61Id6uQ5B7DgEW')
+      .update({
+        object_id: req.body.object_id,
+        object_type: req.body.object_type,
+        aspect_type: req.body.aspect_type,
+        event_time: req.body.event_time,
+        owner_id: req.body.owner_id,
+        subscription_id: req.body.subscription_id,
+        update_date: currentDate,
+        updates: req.body.updates,
+      })
+
+      // If aspect_type is 'create' && run && probably some other things GET strava activity by object ID
+      if (req.body.aspect_type === 'create') {
+        const token = await db.collection('access_tokens').doc('W50yW2KWMFL2U0XJGbru').get()
+        const newestActivity = await fetch(
+          `https://www.strava.com/api/v3/activities/${req.body.object_id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token.data().access_token}`,
+            },
+          },
+        )
+        const activityData = await newestActivity.json()
+        const time = new Date(activityData.start_date_local).getTime()
+
+        // If aspect_type is create then get weather for that time
+        let weather = await getWeather(activityData.start_latlng[0], activityData.start_latlng[1], time/1000)
+        // Form a PUT request to update the new activity with weather info
+        const updateActivity = await fetch(
+          `https://www.strava.com/api/v3/activities/${req.body.object_id}`,
+          {
+            method: 'PUT',
+            headers: {
+              Authorization: `Bearer ${token.data().access_token}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"description": `🚀 Temp: ${Math.round(weather.data[0].temp)}F Dew Point: ${Math.round(weather.data[0].dew_point)}F Felt Like: ${Math.round(weather.data[0].feels_like)}F\r`}),
+            //🏁 Temp: ${Math.round(weather.data[0].temp)}F Dew Point: ${Math.round(weather.data[0].dew_point)}F Felt Like: ${Math.round(weather.data[0].feels_like)}F
+          },
+        )
+
+        console.log(updateActivity)
+      }
+      
+
+      
+      
+      
       res.status(200).json({ message: 'Success!' });
   
     }
