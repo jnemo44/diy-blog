6290fb3d98da642dc5197faa4eb0df5721e11a86
Joe Niemiec <joe.niemiec@gmail.com>
2023-02-10 21:04:13 -0600

    Race date

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,13 @@ import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps}) {
   const [colorTheme, setTheme] = useDarkMode();
   let currentDate = Date.now()
-  let raceDate = new Date("1/15/2023")
+  let raceDate = new Date("3/4/2023")
   return (
     <div className="mx-auto w-10/12 sm:w-8/12 font-Poppins">
       <header className='flex w-full justify-between space-x-4'>
         <Link href="/"><a className="text-2xl font-bold my-4 whitespace-nowrap">Joe Niemiec</a></Link>
-        {/* <div className="hidden font-bold sm:flex place-self-center whitespace-nowrap">Next race: {Math.round((raceDate - currentDate) / (1000 * 3600 * 24))} days </div> */}
-        <a href="https://rtrt.me/ulink/HOUA/HOU-2023/tracker/R5ACUDJW/focus" className="hidden font-bold sm:flex place-self-center whitespace-nowrap" target="_blank" rel="noopener noreferrer">TRACK ME LIVE</a>
+        <div className="hidden font-bold sm:flex place-self-center whitespace-nowrap">Next race: {Math.round((raceDate - currentDate) / (1000 * 3600 * 24))} days </div>
+        {/* <a href="https://rtrt.me/ulink/HOUA/HOU-2023/tracker/R5ACUDJW/focus" className="hidden font-bold sm:flex place-self-center whitespace-nowrap" target="_blank" rel="noopener noreferrer">TRACK ME LIVE</a> */}
         <nav className="place-self-center pr-2">
           <div className="flex space-x-4 font-bold">
             <Link href="/"><a>Home</a></Link>
